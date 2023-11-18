e1167c6da10dc16f94f786a2fce1f8766528d5c1
ray-zero2 <rei2219.matsuda@gmail.com>
2023-11-18 22:07:09 +0900

    some fixes

--- eslint.config.js
+++ eslint.config.js
@@ -1,40 +1,33 @@
-import { FlatCompat } from '@eslint/eslintrc'
-import typescriptEslint from '@typescript-eslint/eslint-plugin'
-import eslintConfigPrettier from 'eslint-config-prettier'
-import js from '@eslint/js'
-import typeScriptESLintParser from '@typescript-eslint/parser'
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import typeScriptESLintParser from '@typescript-eslint/parser';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
-const compat = new FlatCompat()
+const compat = new FlatCompat();
 
 export default [
   {
-    ignores: ['dist']
+    ignores: ['dist'],
   },
   js.configs.recommended,
+  ...compat.extends('standard-with-typescript', 'plugin:@typescript-eslint/eslint-recommended'),
   eslintConfigPrettier,
-  ...compat.extends(
-    'standard-with-typescript',
-    'plugin:@typescript-eslint/eslint-recommended'
-  ),
-  ...compat.plugins(
-    'import',
-    'unused-imports'
-  ),
+  ...compat.plugins('import', 'unused-imports'),
   {
     files: ['src/**/*.ts'],
     plugins: {
-      typescriptEslint
+      typescriptEslint,
     },
     languageOptions: {
       parser: typeScriptESLintParser,
       parserOptions: {
         sourceType: 'module',
-        ecmaVersion: 'latest'
-      }
+        ecmaVersion: 'latest',
+      },
     },
     rules: {
       '@typescript-eslint/triple-slash-reference': 'off',
-      '@typescript-eslint/space-before-function-paren': 'off',
       'unused-imports/no-unused-imports': 'error',
       'import/prefer-default-export': 'off',
       'import/order': [
@@ -45,10 +38,10 @@ export default [
           pathGroupsExcludedImportTypes: ['builtin'],
           alphabetize: {
             order: 'asc',
-            caseInsensitive: true
-          }
-        }
-      ]
-    }
-  }
-]
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,125 +1,127 @@
 export interface AnimationProps {
-  deltaTime: number
-  time: number
+  deltaTime: number;
+  time: number;
 }
 
-export type AnimationFunction = (props: AnimationProps) => void
+export type AnimationFunction = (props: AnimationProps) => void;
 
 export interface Animation {
-  id: string
-  update: AnimationFunction
-  order: number
+  id: string;
+  update: AnimationFunction;
+  order: number;
 }
 
-export function getLerpCoeff (coeff: number, deltaTime: number, targetFps: number = 60): number {
-  const frameStretch = deltaTime * targetFps
-  const adjustedCoeff = 1 - (1 - coeff) ** frameStretch
-  return adjustedCoeff
+export function getLerpCoeff(coeff: number, deltaTime: number, targetFps: number = 60): number {
+  const frameStretch = deltaTime * targetFps;
+  const adjustedCoeff = 1 - (1 - coeff) ** frameStretch;
+  return adjustedCoeff;
 }
 
 class Filmer {
-  private time: number = 0
+  private time: number = 0;
 
-  private deltaTime: number = 0
+  private deltaTime: number = 0;
 
-  private lastTimestamp: number = 0
+  private lastTimestamp: number = 0;
 
-  private animationCount: number = 0
+  private animationCount: number = 0;
 
-  private animationId: ReturnType<typeof requestAnimationFrame> | null = null
+  private animationId: ReturnType<typeof requestAnimationFrame> | null = null;
 
-  private animations: Animation[] = []
+  private animations: Animation[] = [];
 
   get animationList(): Animation[] {
-    return this.animations
+    return this.animations;
   }
 
   get currentTime(): number {
-    return this.time
+    return this.time;
   }
 
   get isAnimating(): boolean {
-    return this.animationId !== null
+    return this.animationId !== null;
   }
 
   getLerpCoeff(coeff: number, targetFPS: number = 60): number {
-    return getLerpCoeff(coeff, this.deltaTime, targetFPS)
+    return getLerpCoeff(coeff, this.deltaTime, targetFPS);
   }
 
   add(id: string, update: AnimationFunction, order?: number): () => void {
-    const needsRestarting = this.animationId !== null
-    if (needsRestarting) this.stop()
+    const needsRestarting = this.animationId !== null;
+    if (needsRestarting) this.stop();
 
-    const newOrder = order ?? this.animationCount
-    this.animations.push({ id, update, order: newOrder })
-    this.sortAnimationsArray()
-    if (needsRestarting) this.start()
-    this.animationCount += 1
-    return () => { this.remove(id) }
+    const newOrder = order ?? this.animationCount;
+    this.animations.push({ id, update, order: newOrder });
+    this.sortAnimationsArray();
+    if (needsRestarting) this.start();
+    this.animationCount += 1;
+    return () => {
+      this.remove(id);
+    };
   }
 
   remove(id: string): void {
-    const needsRestarting = this.animationId !== null
-    if (needsRestarting) this.stop()
-    const newAnimations = this.animations.filter((animation) => animation.id !== id)
-    this.animations = newAnimations
-    if (needsRestarting) this.start()
+    const needsRestarting = this.animationId !== null;
+    if (needsRestarting) this.stop();
+    const newAnimations = this.animations.filter((animation) => animation.id !== id);
+    this.animations = newAnimations;
+    if (needsRestarting) this.start();
   }
 
   removeAll(): void {
-    this.stop()
-    this.animations = []
-    this.animationCount = 0
+    this.stop();
+    this.animations = [];
+    this.animationCount = 0;
   }
 
   start(): void {
-    this.lastTimestamp = performance.now()
-    this.animate()
+    this.lastTimestamp = performance.now();
+    this.animate();
   }
 
   stop(): void {
-    if (typeof this.animationId === 'number') cancelAnimationFrame(this.animationId)
-    this.lastTimestamp = 0
-    this.animationId = null
+    if (typeof this.animationId === 'number') cancelAnimationFrame(this.animationId);
+    this.lastTimestamp = 0;
+    this.animationId = null;
   }
 
   reset(needsAllRemoving: boolean = true): void {
-    this.stop()
-    this.time = 0
-    if (needsAllRemoving) this.removeAll()
+    this.stop();
+    this.time = 0;
+    if (needsAllRemoving) this.removeAll();
   }
 
   reorder(): void {
-    this.sortAnimationsArray()
-    let counter = 0
+    this.sortAnimationsArray();
+    let counter = 0;
     const renumbered = this.animations.map((animation) => {
-      const { order } = animation
-      if (!Number.isFinite(order)) return animation
-      counter += 1
+      const { order } = animation;
+      if (!Number.isFinite(order)) return animation;
+      counter += 1;
       return {
         ...animation,
-        order: counter - 1 // order is 0 indexed
-      }
-    })
-    this.animationCount = renumbered.length
-    this.animations = renumbered
+        order: counter - 1, // order is 0 indexed
+      };
+    });
+    this.animationCount = renumbered.length;
+    this.animations = renumbered;
   }
 
   protected animate(): void {
-    const timestamp = performance.now()
-    this.animationId = requestAnimationFrame(this.animate.bind(this))
-    this.deltaTime = (timestamp - this.lastTimestamp) * 0.001
-    this.time += this.deltaTime
+    const timestamp = performance.now();
+    this.animationId = requestAnimationFrame(this.animate.bind(this));
+    this.deltaTime = (timestamp - this.lastTimestamp) * 0.001;
+    this.time += this.deltaTime;
     this.animations.forEach((animation) => {
-      animation.update({ deltaTime: this.deltaTime, time: this.time })
-    })
-    this.lastTimestamp = timestamp
+      animation.update({ deltaTime: this.deltaTime, time: this.time });
+    });
+    this.lastTimestamp = timestamp;
   }
 
   private sortAnimationsArray(): void {
-    this.animations.sort((a, b) => a.order - b.order)
+    this.animations.sort((a, b) => a.order - b.order);
   }
 }
 
-const filmer = new Filmer()
-export default filmer
+const filmer = new Filmer();
+export default filmer;
